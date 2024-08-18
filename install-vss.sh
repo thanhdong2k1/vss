90d093fbae24f3d2eaa63ea67763200689911994
htdong0510 <donghuynh@antking.asia>
2024-08-18 15:38:12 +0700

    first commit

--- install-vss.sh
+++ install-vss.sh
@@ -1,19 +1,16 @@
 yes | pkg update
 yes | pkg upgrade
 clear
-cd
-cd
 cd $HOME
 pkg install git -y
 git clone https://github.com/thanhdong2k1/vss.git
 cd vss
 unzip vss.zip
-cd vss
 chmod a+x *
 clear
-echo 'PATH="$PATH:$HOME/vss"' >> $HOME/.bashrc 
-source $HOME/.bashrc 
-echo 'PATH="$PATH:$HOME/vss"' >> $HOME/.zshrc 
+echo 'PATH="$PATH:$HOME/vss"' >> $HOME/.bashrc
+source $HOME/.bashrc
+echo 'PATH="$PATH:$HOME/vss"' >> $HOME/.zshrc
 source $HOME/.zshrc
 mv "$HOME/vss/.shortcuts" "$HOME"
 chmod -R a-x,u=rwX,go-rwx /data/data/com.termux/files/home/.shortcuts/icons
